3436279825a065cd3c09a2930de94bfdd99e3cdb
GGbeng <15010976823@163.com>
2023-12-29 20:19:58 +0800

    init: init

---
+++ demo1/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <micro-app
+      class="micro-system"
+      name="system"
+      url="http://localhost:5174"
+      :iframe="true"
+      :keep-alive="true"
+    ></micro-app>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
